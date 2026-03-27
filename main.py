d240a9efc16f9acf6f7f3f2422f59768493bfe94
LuixG-BR <luisgabriel19396@gmail.com>
2026-03-27 16:13:51 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,14 @@
+def calcular_total(quantidade, valor_unitario):
+    return quantidade * valor_unitario
+
+resultado = calcular_total(10,2)
+print(resultado)
+
+def validar_pedido(item, quantidade, valor_unitario):
+    if item and quantidade > 0 and valor_unitario > 0:
+        return "Pedido válido"
+    else:
+        return "Pedido inválido"
+
+print(validar_pedido("",2,0))
+    
